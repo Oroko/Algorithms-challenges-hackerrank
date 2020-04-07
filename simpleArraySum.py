057ac9522d8068faeaaa2d56dff7216532d40dfa
Abel Gechure <aoroko09@gmail.com>
2020-04-07 10:06:46 -0400

    Added a python file, The function takes in two parameters and adds them and returns the result

---
+++ simpleArraySum.py
@@ -0,0 +1,12 @@
+
+#A simple python function that takes two arguments and adds them
+
+
+def simpleArraySum(a,b):
+    return a + b;
+    
+num1 = int(input("Please Enter a number: "));
+num2 = int(input("Please enter a number: "));
+result = simpleArraySum(num1,num2);
+print(result);
+
